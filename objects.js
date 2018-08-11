9920939a9bcb2c336ebd66918ab532f8492c6ab7
Austin Luft <ausluft@gmail.com>
2018-08-11 15:05:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,12 +2,10 @@ var playlist = { artistName: "song" }
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-  
   return playlist
 }
 
 function removeFromPlaylist (playlist, artistName) {
   delete playlist[artistName]
-  
   return playlist
 }
