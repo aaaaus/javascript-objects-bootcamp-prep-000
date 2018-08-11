e19523077ddd54bb23624ebc1cd75f86a0533b60
Austin Luft <ausluft@gmail.com>
2018-08-11 15:09:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { artistName: "song" }
+var playlist = new Object({ artistName: "song" })
 
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
